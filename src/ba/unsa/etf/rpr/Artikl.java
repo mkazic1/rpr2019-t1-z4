b4d53a93a516cc4db32cc0efe049dce40317ddb9
mkazic1 <mkazic1@etf.unsa.ba>
2020-10-30 19:53:39 +0100

    konstruktor, seter

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,17 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    private String naziv, cijena, kod;
+
+    public Artikl(String ime, String koliko, String sifra) {
+        naziv=ime;
+        cijena=koliko;
+        kod=sifra;
+    }
+
+
+
+
+
+
 }
