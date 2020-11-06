cc8739a5d1a08b750697636e6b313c488a01fa7f
mkazic1 <mkazic1@etf.unsa.ba>
2020-11-06 11:57:06 +0100

    getteri i setteri dodani

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -10,4 +10,28 @@ public class Artikl {
         this.cijena = cijena;
         this.kod=kod;
     }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(int cijena) {
+        this.cijena = cijena;
+    }
+
+    public String getKod() {
+        return kod;
+    }
+
+    public void setKod(String kod) {
+        this.kod = kod;
+    }
 }
