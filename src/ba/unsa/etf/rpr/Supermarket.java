c2a07bbc331135f223b42c1b5541d292bcacd441
mkazic1 <mkazic1@etf.unsa.ba>
2020-11-06 13:42:38 +0100

    getter za klasu Supermarket metoda dodavanja artikla u supermarketu

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,12 +1,20 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
-    /*public Artikl[] getArtikli() {
-    }*/
+    private Artikl[] moji_artikli=new Artikl[1000]; // max 1000 artikala
+    private int broj_artikala=0;
 
-    /*public Artikl izbaciArtiklSaKodom(String kod) {
+    public Artikl[] getArtikli() {
+        return moji_artikli;
     }
 
-    public void dodajArtikl(Artikl a) {
-    }*/
+    public boolean dodajArtikl(Artikl a) {
+        if(broj_artikala<1000) {
+            moji_artikli[broj_artikala]=new Artikl(a.getNaziv(), a.getCijena(), a.getKod());
+            broj_artikala++;
+            return true;
+        }
+        return false;
+    } // ako je korpa puna artikl ostaje u supermarketu
+
 }
